2157034058b81dcb850b4757b5d43952167bb1da
hammouda997 <hammouda.msaad@epsrit.tn>
2024-09-18 10:19:10 +0100

    fix errors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Mine from './components/home/mine';
 
 function App() {
-  const [taps, setTaps] = useState(0);
 
   useEffect(() => {
-    // Initialize Telegram WebApp SDK
     const tg = window.Telegram.WebApp;
-    tg.ready();  // Make Telegram app interface ready
-
-    // Optional: Set a custom background color
+    tg.ready(); 
     tg.MainButton.setText('Start Mining').show();
   }, []);
 
